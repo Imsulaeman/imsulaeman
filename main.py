bf117b4fb5f69f60db2194599b8e0714a1acdc6e
Imsulaeman <63634909+Imsulaeman@users.noreply.github.com>
2026-06-26 13:23:33 +0700

    refactor: rewrite terminal as personal commands, not x0rzavi clone

--- main.py
+++ main.py
@@ -4,55 +4,46 @@ import gifos
 
 
 def main():
-    t = gifos.Terminal(750, 420, 15, 15)
+    t = gifos.Terminal(700, 380, 15, 15)
 
     git = gifos.utils.fetch_github_stats("imsulaeman")
     top_langs = [lang[0] for lang in git.languages_sorted]
     year_now = datetime.now(ZoneInfo("Asia/Jakarta")).strftime("%Y")
 
-    # boot straight into prompt
     t.toggle_show_cursor(False)
-    t.gen_text("\x1b[93mBandung OS (tty1)\x1b[0m", 1, count=5)
-    t.gen_text("login: ", 3, count=5)
+    t.gen_text("", 1, count=10)
+
+    # whoami
+    t.gen_prompt(2, count=3)
     t.toggle_show_cursor(True)
-    t.gen_typing_text("imsulaeman", 3, contin=True)
-    t.gen_text("", 4, count=3)
+    t.gen_typing_text("whoami", 2, contin=True)
     t.toggle_show_cursor(False)
-    t.gen_text("password: ", 4)
+    t.gen_text("\x1b[93mIlham Maulana Sulaeman\x1b[0m -- Bandung, Indonesia", 3)
+    t.gen_text("student @ Bina Nusantara |  reads finance, builds tools", 4, count=8)
+
+    # ls projects
+    t.gen_prompt(6, count=3)
     t.toggle_show_cursor(True)
-    t.gen_typing_text("*********", 4, contin=True)
+    t.gen_typing_text("ls projects/", 6, contin=True)
     t.toggle_show_cursor(False)
+    t.gen_text(
+        "\x1b[94mFolio\x1b[0m   \x1b[94mSecond-Brain\x1b[0m   \x1b[94mRutin\x1b[0m   \x1b[94mimsulaeman.me\x1b[0m",
+        7, count=8
+    )
 
-    t.clear_frame()
-
-    t.gen_prompt(1)
-    prompt_col = t.curr_col
-    t.clone_frame(8)
+    # git log
+    t.gen_prompt(9, count=3)
     t.toggle_show_cursor(True)
-    t.gen_typing_text("fetch.sh", 1, contin=True)
+    t.gen_typing_text("git log --oneline -3", 9, contin=True)
     t.toggle_show_cursor(False)
+    t.gen_text(f"\x1b[33m{git.total_commits_last_year}\x1b[0m commits in {int(year_now) - 1}  |  top langs: \x1b[93m{', '.join(top_langs[:3])}\x1b[0m", 10)
+    t.gen_text(f"stars earned: \x1b[93m{git.total_stargazers}\x1b[0m  |  rank: \x1b[93m{git.user_rank.level}\x1b[0m", 11, count=8)
 
-    details = f"""
-    \x1b[30; 101m ilham@github \x1b[0m
-    -------------------------
-    \x1b[96mName    \x1b[0mIlham Maulana Sulaeman
-    \x1b[96mFrom    \x1b[0mBandung, Indonesia
-    \x1b[96mStudy   \x1b[0mBina Nusantara University
-    \x1b[96mWeb     \x1b[0mimsulaeman.me
-
-    \x1b[96mStars   \x1b[93m{git.total_stargazers}\x1b[0m
-    \x1b[96mCommits \x1b[93m{git.total_commits_last_year}\x1b[0m  ({int(year_now) - 1})
-    \x1b[96mLangs   \x1b[93m{", ".join(top_langs[:4])}\x1b[0m
-    -------------------------
-    \x1b[90mReads finance. Builds tools.\x1b[0m
-    \x1b[90mThreads: @ulanghidup\x1b[0m
-    """
-
-    t.gen_text(details, 3, count=5, contin=True)
-
-    t.gen_prompt(t.curr_row)
+    # closing line
+    t.gen_prompt(13, count=3)
     t.toggle_show_cursor(True)
-    t.gen_text("", t.curr_row, count=100, contin=True)
+    t.gen_typing_text("# if it bothers me enough, i'll build it", 13, contin=True)
+    t.gen_text("", 13, count=80, contin=True)
 
     t.gen_gif()
 
